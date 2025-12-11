9f34129276f8762b66685caacadcd4ce46afcbd4
GiridharanMohan <giri28602@gmail.com>
2025-12-11 16:30:21 +0530

    created entities

---
+++ src/main/java/com/dev/jobportal/model/Application.java
@@ -0,0 +1,27 @@
+package com.dev.jobportal.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "applications")
+public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "applicant_id", nullable = false)
+    private Applicant applicant;
+
+    @ManyToOne
+    @JoinColumn(name = "job_id", nullable = false)
+    private Job job;
+}
