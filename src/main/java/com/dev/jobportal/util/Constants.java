b1e25b203eabab81cf73f20a09d710afceaafb06
GiridharanMohan <giri28602@gmail.com>
2025-12-20 17:37:32 +0530

    created constants file for better readability

---
+++ src/main/java/com/dev/jobportal/util/Constants.java
@@ -0,0 +1,8 @@
+package com.dev.jobportal.util;
+
+public class Constants {
+
+    public static final String STATUS_APPLIED = "APPLIED";
+    public static final String STATUS_REJECTED = "REJECTED";
+    public static final String STATUS_SHORTLISTED = "SHORTLISTED";
+}
