52a0709cd0a9ab4d5a9ce218101edb39b141dafa
GiridharanMohan <giri28602@gmail.com>
2026-02-01 17:18:07 +0530

    removed unused import

--- src/main/java/com/dev/jobportal/repository/ApplicationRepository.java
+++ src/main/java/com/dev/jobportal/repository/ApplicationRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
     Page<Application> findAllByJob(Pageable pageable, Job job);
